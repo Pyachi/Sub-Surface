fc63fcefe0b66a1093fd009656a017564c94eca1
MarkEScheidker <mark.scheidker@gmail.com>
2022-09-21 12:37:08 -0500

    added moving gun that follows mouse needs some work

--- Assets/SubBehavior.cs
+++ Assets/SubBehavior.cs
@@ -1,18 +1,19 @@
 ﻿using System;
+using System.Data.Common;
 using System.Drawing;
 using UnityEngine;
 
 public class SubBehavior : MonoBehaviour
 {
 
-    private Rigidbody _sub;
-    private Rigidbody _camera;
+    public Rigidbody sub;
+    public GameObject gunPivot;
+    public Rigidbody camera;
     private void Start()
     {
         AudioManager.Play("submarine_ambience"); 
-        _sub = GetComponent<Rigidbody>();
-        _camera = FindObjectOfType<Camera>().GetComponent<Rigidbody>();
         
+
     }
 
     private void OnCollisionEnter(Collision collision)
@@ -24,25 +25,25 @@ public class SubBehavior : MonoBehaviour
     public void FixedUpdate()
     {
         //get current position of objects in submarine
-        var pos1 = _sub.transform.position;
+        var pos1 = sub.transform.position;
         var subX = pos1.x;
         var subY = pos1.y;
-        var pos2 = _camera.transform.position;
+        var pos2 = camera.transform.position;
         var cameraX = pos2.x;
         var cameraY = pos2.y;
 
         //move camera towards sub body
-        _camera.AddForce(new Vector3((subX - cameraX) * 2, (subY - cameraY) * 2, 0));
+        camera.AddForce(new Vector3((subX - cameraX) * 2, (subY - cameraY) * 2, 0));
 
         //move sub body in direction of player input
         if (Input.GetKey(KeyCode.A))
-            _sub.AddForce(new Vector3(2, 0, 0));
+            sub.AddForce(new Vector3(2, 0, 0));
         if (Input.GetKey(KeyCode.D))
-            _sub.AddForce(new Vector3(-2, 0, 0));
+            sub.AddForce(new Vector3(-2, 0, 0));
         if (Input.GetKey(KeyCode.W))
-            _sub.AddForce(new Vector3(0, 2, 0));
+            sub.AddForce(new Vector3(0, 2, 0));
         if (Input.GetKey(KeyCode.S))
-            _sub.AddForce(new Vector3(0, -2, 0));
+            sub.AddForce(new Vector3(0, -2, 0));
         if (Input.GetKey(KeyCode.F))
             AudioManager.PlayOneShot("SubBump");
             
@@ -50,32 +51,32 @@ public class SubBehavior : MonoBehaviour
         //snap camera and body to other side of level if moved there
         if (subX > 25)
         {
-            _sub.transform.position += new Vector3(-50, 0, 0);
-            _camera.transform.position += new Vector3(-50, 0, 0);
+            sub.transform.position += new Vector3(-50, 0, 0);
+            camera.transform.position += new Vector3(-50, 0, 0);
         }
 
         if (subX < -25)
         {
-            _sub.transform.position += new Vector3(50, 0, 0);
-            _camera.transform.position += new Vector3(50, 0, 0);
+            sub.transform.position += new Vector3(50, 0, 0);
+            camera.transform.position += new Vector3(50, 0, 0);
         }
 
         //add stop sub above surface
         if (subY > 24.75)
-            _sub.AddForce(new Vector3(0, (float)(subY - 24.75) * -4, 0));
+            sub.AddForce(new Vector3(0, (float)(subY - 24.75) * -4, 0));
         //stop sub at seafloor
         if (subY < -24.75)
             //_sub.transform.position = new Vector3(subX, (float)-24.75, 0);
         //raise camera near surface
         if(cameraY > 24)
-            _camera.AddForce(new Vector3(0,(cameraY - 24) * 1, 0));
+            camera.AddForce(new Vector3(0,(cameraY - 24) * 1, 0));
         //stop camera near seafloor
         if(cameraY < -15)
-            _camera.AddForce(new Vector3(0,  (cameraY + 15) * (float)-1.2, 0));
+            camera.AddForce(new Vector3(0,  (cameraY + 15) * (float)-1.2, 0));
         
         //change pitch and volume of submarine based on speed
-        AudioManager.SetPitch("submarine_ambience", ((_sub.velocity.magnitude)/8) + 1 );
-        AudioManager.SetVolume("submarine_ambience", ((_sub.velocity.magnitude)/8) + (float)0.1);
+        AudioManager.SetPitch("submarine_ambience", ((sub.velocity.magnitude)/8) + 1 );
+        AudioManager.SetVolume("submarine_ambience", ((sub.velocity.magnitude)/8) + (float)0.1);
         
         //set fog color based on height
         if (cameraY < 25)
@@ -90,4 +91,13 @@ public class SubBehavior : MonoBehaviour
 
     }
 
+    private void Update()
+    {
+        Vector2 mousePos = Input.mousePosition;
+        Vector2 screenCenter = new Vector2(Screen.width * 0.5f, Screen.height * 0.5f);
+        mousePos -= screenCenter;
+        gunPivot.transform.eulerAngles = new Vector3(0,0, 360 - Mathf.Atan2(-mousePos.x, mousePos.y) * Mathf.Rad2Deg);
+        //Debug.Log("it's running");
+
+    }
 }
