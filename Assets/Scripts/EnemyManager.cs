3b8db6681a3edeb3cedb547ec576152ffedc168e
MarkEScheidker <mark.scheidker@gmail.com>
2022-11-14 11:48:44 -0600

    enemies no longer spawn on screen while close to water surface or seafloor

--- Assets/Scripts/EnemyManager.cs
+++ Assets/Scripts/EnemyManager.cs
@@ -22,18 +22,40 @@ public static class EnemyManager
     {
         _lifetime++;
         _credits += 0.02; //To be scaled with time, credits are spent to spawn enemies, stronger enemies -> more credit cost
-        if (_credits < 1.0) return;
+        if (_credits < 1.0) //if there are not enough credits to spawn an enemy, then return
+        {
+            return;
+        }
+        
         _credits -= 1.0;
-        var rand = Random.Range(0, 10);
+        var rand = Random.Range(0, 10);  //pick a random number that corresponds to a specific fish
         GameObject selection;
-        if (rand < 6) selection = Fish;
-        else if (rand < 9) selection = Shark;
-        else selection = Eel;
-        var locAngle = Random.Range(0F, 2F * (float)Math.PI);
-        var spawnPosition = _sub.transform.position +
-                            new Vector3((float)(Math.Sin(locAngle) * 15), (float)(Math.Cos(locAngle) * 15), 0F);
-        if (spawnPosition.y > 24F) spawnPosition.y = 24F;
-        else if (spawnPosition.y < -24F) spawnPosition.y = -24F;
-        ObjectManager.AddObject(Object.Instantiate(selection, spawnPosition, Quaternion.identity));
+        
+        if (rand < 6) //pick a fish
+        {
+            selection = Fish;
+        }
+        else if (rand < 9)
+        {
+             selection = Shark;
+        }
+        else
+        {
+            selection = Eel;
+        }
+
+        Vector3 spawnPosition;
+        while (true) //pick a random point off the screen and spawn an enemy, if the enemy is out of bounds try again.
+        {
+            var locAngle = Random.Range(0F, 2F * (float)Math.PI); //
+            spawnPosition = _sub.transform.position + new Vector3((float)(Math.Sin(locAngle) * 15), (float)(Math.Cos(locAngle) * 15), 0F);
+
+            if (spawnPosition.y < 24F && spawnPosition.y > -24F)  //try again if out of bounds
+            {
+                break;
+            }
+        }
+
+        ObjectManager.AddObject(Object.Instantiate(selection, spawnPosition, Quaternion.identity));  //instantiate the enemy and add it to object list
     }
 }
