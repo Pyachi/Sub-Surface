f560b06c61159e39a0f823299eb7096066cc5757
MarkEScheidker <mark.scheidker@gmail.com>
2022-11-16 11:45:53 -0600

    added bad stuff that doesn't work for enemy ai, pushing so things are synchronized

--- Assets/Scripts/EnemyBehaviors/FishBehavior.cs
+++ Assets/Scripts/EnemyBehaviors/FishBehavior.cs
@@ -11,13 +11,19 @@ public class FishBehavior : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        thisrigidbody = this.gameObject.GetComponent<Rigidbody>();
+        
 
     }
 
     private void FixedUpdate()
     {
-
-
+        thisrigidbody = this.gameObject.GetComponent<Rigidbody>();
+        float suby = sub.position.y;
+        float subx = sub.position.x;
+        float thisy = thisrigidbody.position.y;
+        float thisx = thisrigidbody.position.x;
+        thisrigidbody.transform.eulerAngles = new Vector3(0, 0,
+            Mathf.Atan2((suby - thisy), (subx - thisx)) *
+            Mathf.Rad2Deg);
     }
 }
