711b59192e5bf51721e9b2620d886fd500b5724c
MarkEScheidker <mark.scheidker@gmail.com>
2022-11-16 18:11:55 -0600

    actually added new scripts for functionality... oops

---
+++ Assets/Scripts/BombBehavior.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+
+public class BombBehavior: MonoBehaviour
+{
+    private Rigidbody thisrigidbody;
+    // Start is called before the first frame update
+    void Start()
+    {
+       thisrigidbody = this.gameObject.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            Vector3 subpos = Core.SubObject.transform.position;
+            subpos.y -= 0.9f;
+            Vector3 difference = subpos - this.transform.position;
+            float distance = difference.magnitude;
+            
+            if (distance < 1.5 && distance > 1)
+            {
+                thisrigidbody.AddForce((difference*10) / (distance * 2.0f));
+            }
+            else if (distance < 1)
+            {
+                thisrigidbody.AddForce(difference*15);
+            }
+        }
+    }
+
+    void countdown()
+    {
+        
+    }
+
+    private void particles()
+    {
+        
+    }
+
+    private void explode()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/BombManager.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using UnityEngine;
+using Object = UnityEngine.Object;
+using Random = UnityEngine.Random;
+
+public static class BombManager
+{
+    private static readonly GameObject Bomb = Resources.Load("spawnables/Bomb", typeof(GameObject)) as GameObject;
+    private static int _lifetime;
+    private static double _credits;
+
+    public static void Update()
+    {
+        _lifetime++;
+        _credits += 0.02; //To be scaled with time, credits are spent to spawn bombs
+        if (_credits < 10.0) //if there are not enough credits to spawn a bomb, then return
+        {
+            return;
+        }
+        _credits -= 10.0;
+        
+        Vector3 spawnPosition;
+        while (true) //pick a random point off the screen and spawn a bomb, if the bomb is out of bounds try again.
+        {
+            var locAngle = Random.Range(0F, 2F * (float)Math.PI); //
+            spawnPosition = Core.SubObject.transform.position + new Vector3((float)(Math.Sin(locAngle) * 15), (float)(Math.Cos(locAngle) * 15), 0F);
+
+            if (spawnPosition.y < 24F && spawnPosition.y > -24F)  //try again if out of bounds
+            {
+                break;
+            }
+        }
+        
+        ObjectManager.AddObject(Object.Instantiate(Bomb, spawnPosition, Quaternion.identity));  //instantiate the bomb and add it to object list
+    }
+}
